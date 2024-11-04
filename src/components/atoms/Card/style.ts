d749a642ea1cdc281ee49e96c33346e9188be660
Romulo Carvalho <romulocarvalho081@gmail.com>
2024-11-04 00:06:52 -0300

    style: center content on the right

--- src/components/atoms/Card/style.ts
+++ src/components/atoms/Card/style.ts
@@ -6,10 +6,7 @@ export const Wrapper = styled.main`
   height: 100%;
   padding: 1.25rem;
   border-radius: 1.25rem;
-  text-align: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   box-shadow: 0px 4px 4px 0px #00000040;
 `
